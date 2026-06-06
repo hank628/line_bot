8dd601ca45512f9cc7e9973f8a2e28e9c239275a
hank628 <gmleisure@gm.nttu.edu.tw>
2026-06-06 19:51:03 +0800

    Update line_bot.py

--- line_bot.py
+++ line_bot.py
@@ -122,4 +122,4 @@ def internal_error(error):
 # ========== 主程式 ==========
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=True)   # fix indent
